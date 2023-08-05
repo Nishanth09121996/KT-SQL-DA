52ee71bac0a9930d9a79b65d9a77d65fc7837c82
Nishanth09121996 <35561045+Nishanth09121996@users.noreply.github.com>
2023-08-05 21:25:40 +0530

    New Scripts

--- KTdocuments/reference clips.sql
+++ KTdocuments/reference clips.sql
@@ -13,3 +13,24 @@ FOREIGN KEY (team1_id) REFERENCES ipldisconnected.teams(id);
 
 Alter table ipldisconnected.ipl_matches
 drop constraint FK_player_id;
+
+
+Alter table ipl.venues
+Add constraint primary_key_col
+primary key (id);
+
+Alter table ipl.players
+Add constraint primary_key_col
+primary key (id);
+
+Alter table ipl.seasons
+Add constraint primary_key_col
+primary key (id);
+
+Alter table ipl.teams
+Add constraint primary_key_col
+primary key (id);
+
+Alter table ipl.umpires
+Add constraint primary_key_col
+primary key (id);
